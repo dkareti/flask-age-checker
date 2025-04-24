e4c59b24a441311ca65283486930ff51a1e3a66f
Daniel Kareti <danielkareti@gmail.com>
2025-04-24 11:35:52 -0700

    Created an ml file, to integrate the flask app and an ml model

---
+++ ml_model.py
@@ -0,0 +1,12 @@
+#Daniel Kareti
+#Created on 4/24/25
+
+#####
+
+#This file determines the age group of the user
+
+#####################
+
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.preprocessing import LabelEncoder
